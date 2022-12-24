4d9f029b7e77e69ccd409f14931e84c9b995069d
Charan Vignesh NR <charanvignesh10@gmail.com>
2022-12-24 23:30:27 +0530

    added state component in message.js

--- react-demo/src/components/Message.js
+++ react-demo/src/components/Message.js
@@ -1,4 +1,6 @@
-export const Message = (props) =>
+import {useState} from 'react'
+export const Message = () =>
 {
+    const [message,setMessage]=useState()
     return <h1>Welcome visitor!</h1>
 }
